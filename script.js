b288c455d66b64e64548f7c50876107646e2c996
MamunOfficeLaptop <mamun166036@gmail.com>
2024-05-18 19:20:01 +0600

    uu

---
+++ script.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', () => {
+    //variable
+    const searchInput = document.getElementById('txtSearchProduct');
+    const searchContainer = document.getElementById('searchContainer');
+    const searchCards = document.querySelectorAll('.search-card');
+    //end variable
+
+    searchCards.forEach(e => {
+        e.addEventListener('click', () => window.location.href = '/pages/otherPage.html')
+    });
+
+    document.addEventListener('click', (e) => {
+        if (e.target != searchInput) {
+            searchContainer.style.display = "none";
+        }
+    });
+
+    searchInput.addEventListener('input', (e) => {
+        const value = e.target.value;
+        if (value.length > 2) {
+            searchContainer.style.display = "block";
+        }
+        if (value.length == 0) {
+            searchContainer.style.display = "none";
+        }
+    })
+
+    const slider = tns({
+        container: '#productSlider',
+        items: 1,
+        slideBy: "page",
+        mouseDrag: true,
+        swipeAngle: false,
+        speed: 400,
+        nav: true,
+        autoplay: true,
+        loop: true,
+        lazyload: true,
+        navContainer: false,
+        responsive: {
+            640: {
+                items: 2,
+            },
+
+            768: {
+                items: 4,
+            }
+        }
+    });
+});
